8b2d45f75e06abe74129cfb009791e6fb5feea3a
Abel Adisu <abeladisu73@gmail.com>
2024-08-14 17:19:40 +0300

    Experince section link is added

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import {useState, useRef, Suspense} from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import React from "react";
+import { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 
 const Stars = () => {
-  return (
-    <div>Stars</div>
-  )
-}
+  return <div>Stars</div>;
+};
 
-export default Stars
+export default Stars;
--- src/constants/index.js
+++ src/constants/index.js
@@ -35,6 +35,10 @@ export const navLinks = [
     title: "Work",
   },
   {
+    id: "experience",
+    title: "Experience",
+  },
+  {
     id: "contact",
     title: "Contact",
   },
